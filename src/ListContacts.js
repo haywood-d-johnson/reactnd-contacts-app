363c68b36ffdb659d2ce5aad131e21cb09a5dc65
haywood-d-johnson <haywood.d.johnson@gmail.com>
2020-08-24 00:55:15 -0500

    added propTypes

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
 const ListContacts = props => {
     return (
@@ -27,11 +28,9 @@ const ListContacts = props => {
     );
 }
 
-// class ListContacts extends Component {
-//     render() {
-//         const { contacts } = this.props;
-
-//     }
-// }
+ListContacts.propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired
+};
 
 export default ListContacts;
