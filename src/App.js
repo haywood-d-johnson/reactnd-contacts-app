60dddf9ee20433e631c1fb8e9ccf2c891f89d541
haywood-d-johnson <haywood.d.johnson@gmail.com>
2020-09-12 01:54:29 -0500

    mvp. Project adds contacts

--- src/App.js
+++ src/App.js
@@ -27,6 +27,15 @@ class App extends Component {
     ContactsAPI.remove(contact);
   };
 
+  createContact = contact => {
+    ContactsAPI.create(contact)
+      .then(contact => {
+        this.setState((currentState) => ({
+          contacts: currentState.contacts.concat([contact])
+        }))
+      })
+  }
+
   render() {
     return (
       <div>
@@ -36,7 +45,13 @@ class App extends Component {
             onDeleteContact={this.removeContact}
           />
         )} />
-        <Route path="/create" component={CreateContact} />
+        <Route path="/create" render={() => (
+          <CreateContact
+            onCreateContact={contact => {
+              this.createContact(contact);
+            }}
+          />
+        )} />
       </div>
     );
   }
