f6653b51fde388d249191053a14faa87b3595172
haywood-d-johnson <haywood.d.johnson@gmail.com>
2020-09-12 01:15:05 -0500

    added page routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route } from "react-router-dom";
 
 import ListContacts from "./ListContacts";
 import CreateContact from "./CreateContact";
@@ -7,8 +8,7 @@ import * as ContactsAPI from "./utils/ContactsAPI";
 
 class App extends Component {
   state = {
-    contacts: [],
-    screen: "list"
+    contacts: []
   }
 
   componentDidMount = () => {
@@ -30,20 +30,13 @@ class App extends Component {
   render() {
     return (
       <div>
-        {this.state.screen === "list" && (
+        <Route exact path='/' render={() => (
           <ListContacts
             contacts={this.state.contacts}
             onDeleteContact={this.removeContact}
-            onNavigate={() => {
-              this.setState(() => ({
-                screen: "create"
-              }))
-            }}
           />
-        )}
-        {this.state.screen === "create" && (
-          <CreateContact />
-        )}
+        )} />
+        <Route path="/create" component={CreateContact} />
       </div>
     );
   }
