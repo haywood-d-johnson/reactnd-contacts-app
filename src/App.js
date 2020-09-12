c43903be0368067cb537bf21d3ead45de5357c2c
haywood-d-johnson <haywood.d.johnson@gmail.com>
2020-09-11 23:50:52 -0500

    added react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
 import ListContacts from "./ListContacts";
+import CreateContact from "./CreateContact";
 import * as ContactsAPI from "./utils/ContactsAPI";
+
 
 class App extends Component {
   state = {
-    contacts: []
+    contacts: [],
+    screen: "list"
   }
 
   componentDidMount = () => {
@@ -27,10 +30,20 @@ class App extends Component {
   render() {
     return (
       <div>
-        <ListContacts
-          contacts={this.state.contacts}
-          onDeleteContact={this.removeContact}
-        />
+        {this.state.screen === "list" && (
+          <ListContacts
+            contacts={this.state.contacts}
+            onDeleteContact={this.removeContact}
+            onNavigate={() => {
+              this.setState(() => ({
+                screen: "create"
+              }))
+            }}
+          />
+        )}
+        {this.state.screen === "create" && (
+          <CreateContact />
+        )}
       </div>
     );
   }
